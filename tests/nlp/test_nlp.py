d35d433947d0f209f776dbe1e12b88dd73306453
melihakdag <akdag.melih@gmail.com>
2025-12-03 09:59:32 +0100

    Added a sample text file.

---
+++ tests/nlp/test_nlp.py
@@ -0,0 +1,11 @@
+import nltk
+
+sample_text = """ These visions faded when I perused, for the first time, those poets
+whose effusions entranced my soul and lifted it to heaven. I also
+became a poet and for one year lived in a paradise of my own creation;
+I imagined that I also might obtain a niche in the temple where the
+names of Homer and Shakespeare are consecrated. You are well
+acquainted with my failure and how heavily I bore the disappointment.
+But just at that time I inherited the fortune of my cousin, and my
+thoughts were turned into the channel of their earlier bent."""
+
